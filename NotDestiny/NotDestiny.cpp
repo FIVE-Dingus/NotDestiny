3ce52e22f0be58626a0c7ffec8b6e0c5d1030487
William-Gabali <williamgabali@gmail.com>
2023-10-23 17:14:07 +0200

    m

--- NotDestiny/NotDestiny.cpp
+++ NotDestiny/NotDestiny.cpp
@@ -1,9 +1,8 @@
 // NotDestiny.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
 
-#include <iostream>;
 //#include <SDL.h>;
-#include <iostream>
+#include <iostream>;
 using namespace std;
 
 void printarray(int arg[], int length) {
