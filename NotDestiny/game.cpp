db8fd14cef48bea5dab62d2ec6e6c2a75ba82abf
TranquillouMAN <beney7189@gmail.com>
2023-10-23 18:02:11 +0200

    compréhension

---
+++ NotDestiny/game.cpp
@@ -0,0 +1,15 @@
+#include "game.h";
+#include <iostream>;
+
+Tile::Tile(int x, int y) {
+	
+};
+
+int Tile::GedId(int x, int y) {
+	
+};
+
+void Tile::ValueFromCoord(int x, int y, int value) {
+
+};
+
--- NotDestiny/game.h
+++ NotDestiny/game.h
@@ -1,1 +1,16 @@
 #pragma once
+
+
+class Tile
+{
+private:
+	int x;
+	int y;
+	int value;
+public:
+	Tile(int x, int y);
+
+	int GedId(int x, int y);
+	void ValueFromCoord(int x, int y, int value);
+
+};
